dae14a66540891226749c1a3505d5d4f1b8051b1
EdmarSantosM <santoem4@hotmail.com>
2020-12-29 19:37:18 -0300

    Update App.js
    Estilizando  o  App
    
    importando  o StyleSheet
    
    criado  a page principal, aplicando uma cor de fundo  e  o  flex =1.
    
    Adicionado  uma  View ( importando )

--- appCurriculo/src/App.js
+++ appCurriculo/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 const App = () => {
     return (
         <>
-          
+          <View style={style.page}>
+
+          </View>
         </>
     );
 };
+
+const style = StyleSheet.create({
+
+    page: {
+        backgroundColor: '#E7E7E7',
+        flex: 1
+    }
+})
 
 export default App;
