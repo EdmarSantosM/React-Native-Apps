719f638ea8479a1535470b07ff907f70f02b271b
EdmarSantosM <santoem4@hotmail.com>
2020-12-30 11:38:05 -0300

    Update App.js
    Reestruturando  o projeto

--- appCurriculo/src/App.js
+++ appCurriculo/src/App.js
@@ -1,68 +1,13 @@
 
+import React from 'react';
+import {View, Text} from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-
-import dima from './assets/dima.jpg';
-
-import Sobre from './src/pages/Sobre';
-
-
-const Stack = createStackNavigator();
-
-const App = () => {
+export default function App(){
     return (
-        <>
-          <NavigationContainer>
-                     <Stack.Navigator>
-                        <Stack.Screen name="Sobre" component={Sobre} />
-                    </Stack.Navigator>
-                </NavigationContainer>
-          <View style={style.page}>
-
-              {/*
-                <View style={style.container_cabecalho}>
-                    <Image source={dima} style={style.dima} />
-                    <Text style={style.nome}>Edmar Santos</Text>
-                    <Text style={style.funcao}>Desenvolvedor Júnior</Text>
-              
-                </View>
-                */}
-          </View>
-        </>
+        <View>
+            <Text> App</Text>
+        </View>
     );
-};
-
-const style = StyleSheet.create({
-
-    page: {
-        backgroundColor: '#E7E7E7',
-        flex: 1
-    },
-    container_cabecalho: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    dima: {
-        width: 150,
-        height: 150,
-        borderRadius:150
-    },
-    nome: {
-        fontSize: 26,
-        fontWeight: 'bold',
-        marginTop: 10
-    },
-    funcao: {
-        color: '#939393',
-        marginBottom: 10
-
-    }
+}
 
 
-
-})
-
-export default App;
