3888cd5558179804d1a401fd5351d70e2c32baa0
EdmarSantosM <santoem4@hotmail.com>
2020-12-30 05:32:19 -0300

    Update pastas
    Corpo / estrutura  das pasta

---
+++ appCurriculo/src/pages/Resumo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Resumo(){
+    return (
+        <View>
+            <Text> Resumo </Text>
+        </View>
+    );
+}
