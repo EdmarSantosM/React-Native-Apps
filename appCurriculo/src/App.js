36e3ec84f700b727d0b347fb625d936fba8896f9
EdmarSantosM <santoem4@hotmail.com>
2020-12-30 13:16:51 -0300

    Inclusão da  imagem /  foto
    import dima from './assets/dima.jpg';

--- appCurriculo/src/App.js
+++ appCurriculo/src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import { StyleSheet, View, Text, Image} from 'react-native';
+
 import{NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -9,25 +10,35 @@ import Resumo from './pages/Resumo';
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
 
+
+import dima from './assets/dima.jpg';
+
 const Stack = createStackNavigator();
 
 export default function App(){
     return (
-        <NavigationContainer>
-           <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={Home}
-                    options={{
-                        title: 'Bem-vindo',
-                        headerStyle:{
-                            backgroundColor: '#121212'
-                        },
-                        headerTintColor: '#FFF'
-                    }} />
-                <Stack.Screen name="Resumo" component={Resumo} />
-                <Stack.Screen name="Sobre" component={Sobre} />
-                <Stack.Screen name="Contato" component={Contato} />
-           </Stack.Navigator>
-        </NavigationContainer>
+        <>
+                    <View>
+                        <Image source={dima} />
+                    </View>
+           
+                <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home">
+                        <Stack.Screen name="Home" component={Home}
+                            options={{
+                                title: 'Bem-vindo ao  meu AppCurriculo',
+                                headerStyle:{
+                                    backgroundColor: '#121212'
+                                },
+                                headerTintColor: '#FFF'
+                            }} />
+                        <Stack.Screen name="Resumo" component={Resumo} />
+                        <Stack.Screen name="Sobre" component={Sobre} />
+                        <Stack.Screen name="Contato" component={Contato} />
+                </Stack.Navigator>
+                </NavigationContainer>
+
+        </>
     );
 }
 
--- appCurriculo/src/pages/Home/index.js
+++ appCurriculo/src/pages/Home/index.js
@@ -2,15 +2,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 
+
 export default function Home({navigation}){
     return (
-        <View>
-            <Text>  AppCurrículo </Text>
-            <Button  title="Resumo" onPress={ () => navigation.navigate('Resumo')}/>
-            <Text></Text>
-            <Button  title="Sobre" onPress={ () => navigation.navigate('Sobre')}/>
-            <Text></Text>
-            <Button  title="Contato" onPress={ () => navigation.navigate('Contato')}/>
-        </View>
+        <>
+            <View>
+                <Text></Text>
+                <Button  title="Resumo" onPress={ () => navigation.navigate('Resumo')}/>
+                <Text></Text>
+                <Button  title="Sobre" onPress={ () => navigation.navigate('Sobre')}/>
+                <Text></Text>
+                <Button  title="Contato" onPress={ () => navigation.navigate('Contato')}/>
+            </View>
+               
+        </>
     );
 }
