227f0feaad539871de9eb914cc3a203422140177
EdmarSantosM <santoem4@hotmail.com>
2020-12-30 11:57:35 -0300

    Update App.js
    Importado e incluido as telas
    Home / Resumo/ Sobre

--- appCurriculo/src/App.js
+++ appCurriculo/src/App.js
@@ -4,13 +4,21 @@ import {View, Text} from 'react-native';
 import{NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import Home from './pages/Home';
+import Resumo from './pages/Resumo';
+import Sobre from './pages/Sobre';
+
 const Stack = createStackNavigator();
 
 export default function App(){
     return (
-        <View>
-            <Text> App</Text>
-        </View>
+        <NavigationContainer>
+           <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Resumo" component={Resumo} />
+                <Stack.Screen name="Sobre" component={Sobre} />
+           </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
